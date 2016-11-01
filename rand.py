affc3b9eaf867ddfdae64c4a4db802fabe2d45a0
vishal kumar <vk0193157@gmail.com>
2016-11-01 21:28:17 +0530

    Update rand.py

--- rand.py
+++ rand.py
@@ -6,7 +6,7 @@ with a random intiger between
 6 and 15 and return it"""
 import random
 def fun(name):
-    return name+str(random.randint(6,15))
+    return name+" "+str(random.randint(6,15))
 
 #this is used for calling fun
 if __name__=="__main__":
