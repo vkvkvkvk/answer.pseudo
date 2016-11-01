e8f667aa45512f088bff57b08885e1e2c8619f18
vishal kumar <vk0193157@gmail.com>
2016-11-01 21:21:45 +0530

    Create rand.py

---
+++ rand.py
@@ -0,0 +1,14 @@
+"""
+this is a python module
+which takes name as input
+and concatenate the name
+with a random intiger between 
+6 and 15 and return it"""
+import random
+def fun(name):
+    return name+str(random.randint(6,15))
+
+#this is used for calling fun
+if __name__=="__main__":
+    print(fun(input()))
+
